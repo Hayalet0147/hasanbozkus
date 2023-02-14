83e3e534c9cc52d6d36b06c5ccff84bc5e676979
Hayalet0147 <103758391+Hayalet0147@users.noreply.github.com>
2023-02-14 23:46:45 +0300

    Hasan BOZKUŞ tarafından hasanbozkus.com.tr adresi adı altında canlıya yayınlanmıştır.

---
+++ BusinnesLayer/Concrete/NewsLetterManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinnesLayer.Abstract;
+using DataAccsessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinnesLayer.Concrete
+{
+    public class NewsLetterManager: INewsLetterService
+    {
+        INewsLetterDal _newsLetterDal;
+
+        public NewsLetterManager(INewsLetterDal newsLetter)
+        {
+            _newsLetterDal = newsLetter;
+        }
+
+        public void AddNewsLetter(NewsLetter newsLetter)
+        {
+            _newsLetterDal.Insert(newsLetter);
+        }
+    }
+}
